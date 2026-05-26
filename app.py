0c316e73f0ee4b58cfb92bd5dc146eba63de169e
namansacher1524 <namansacher1524@gmail.com>
2026-05-27 01:33:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,6 +84,8 @@ def resolve_complaint(complaint_id):
             })
 
     return jsonify({"message": "Complaint Not Found"}), 404
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
